67f3ed3020ecfc8c8914fe9e5535af4c72c7e1a0
janbuca369 <firewkifle2@gmail.com>
2025-01-29 22:59:31 +0300

    All of the project can be done

--- 3 - Project Dice Roll Simulator/js/script.js
+++ 3 - Project Dice Roll Simulator/js/script.js
@@ -7,6 +7,10 @@ const buttonE1 = document.getElementById("roll-button");
 
 const diceE1 = document.getElementById("dice");
 
+const rollHistoryE1 = document.getElementById("roll-history");
+
+let historyList = [];
+
 function rollDice() {
   const rollResult = Math.floor(Math.random() * 6) + 1;
   // console.log(rollResult); // chaeking
@@ -15,7 +19,21 @@ function rollDice() {
 
     // diceE1.textContent = diceFace; // Display number
     diceE1.innerHTML = diceFace;
+    historyList.push(rollResult);
+    updateRollHistory();
   
+}
+
+function updateRollHistory(){
+    rollHistoryE1.innerHTML = ""
+    for (let i = 0; i < historyList.length; i++) {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `Roll ${i + 1}.
+         <span>${getDiceFace(
+          historyList[i]
+        )}</span>`;
+        rollHistoryE1.appendChild(listItem)
+    }
 }
 
 function getDiceFace(rollResult) {
