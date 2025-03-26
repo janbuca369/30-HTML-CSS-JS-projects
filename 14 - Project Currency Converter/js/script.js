97456818da8a29054d7c06fcd4d3ec30e826d6d2
janbuca369 <firewkifle2@gmail.com>
2025-03-26 13:01:16 +0300

    js part can be begin

---
+++ 14 - Project Currency Converter/js/script.js
@@ -0,0 +1,9 @@
+const currencyFirestEl = document.getElementById("currency-firest");
+
+const worthFirestEl = document.getElementById("worth-firest");
+
+const currencySecondEl = document.getElementById("currency-second");
+
+const worthSecondEl = document.getElementById("worth-second");
+
+const exchangeRateEl = document.getElementById("exchange-rate");
